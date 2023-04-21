b40280ba1bd543a001f30237eed1e2cca74f3a24
Deividas Zaremba <zarembadeividas90@gmail.com>
2023-04-21 12:50:56 +0300

    fixed view for event list

--- library/views.py
+++ library/views.py
@@ -5,6 +5,7 @@ from .models import WowChar, WowClass, WowPlayer, WowSpec, CharInstance, EventRe
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.db.models import Q
+import calendar
 
 def index(request):
     num_chars = WowChar.objects.all().count()
@@ -53,3 +54,49 @@ def search(request):
     query = request.GET.get('query')
     search_results = WowPlayer.objects.filter(Q(nickname__icontains=query))
     return render(request, 'search.html', {'entries': search_results, 'query': query})
+
+def calendar_view(request, year, month):
+    # Convert year and month strings to integers
+    year = int(year)
+    month = int(month)
+    
+    # Get the calendar for the specified month
+    cal = calendar.monthcalendar(year, month)
+    
+    # Create a list of lists to store the calendar days and their CSS classes
+    days = []
+    for week in cal:
+        week_days = []
+        for day in week:
+            # Determine if this day is in the current month or a previous/next month
+            if day == 0:
+                week_days.append((' ', 'other-month'))
+            elif day < 10:
+                week_days.append((str(day), 'current-month single-digit'))
+            else:
+                week_days.append((str(day), 'current-month'))
+        days.append(week_days)
+    
+    # Get the previous and next months
+    prev_month = month - 1
+    prev_year = year
+    if prev_month == 0:
+        prev_month = 12
+        prev_year = year - 1
+    
+    next_month = month + 1
+    next_year = year
+    if next_month == 13:
+        next_month = 1
+        next_year = year + 1
+    
+    # Render the calendar template with the calendar and paging information
+    return render(request, 'event_list.html', {
+        'year': year,
+        'month': month,
+        'days': days,
+        'prev_month': prev_month,
+        'prev_year': prev_year,
+        'next_month': next_month,
+        'next_year': next_year,
+    })
